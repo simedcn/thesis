6770f851a885f1e01fc3224a22c5cb7f0f1c52b5
chluo <chluo@ebay.com>
2013-01-18 17:09:13 +0800

    add HBase

--- HdfsParent/zookeeper/src/main/java/org/apache/jute/OutputArchive.java
+++ HdfsParent/zookeeper/src/main/java/org/apache/jute/OutputArchive.java
@@ -27,21 +27,34 @@ import java.util.TreeMap;
  *
  */
 public interface OutputArchive {
-    public void writeByte(byte b, String tag) throws IOException;
-    public void writeBool(boolean b, String tag) throws IOException;
-    public void writeInt(int i, String tag) throws IOException;
-    public void writeLong(long l, String tag) throws IOException;
-    public void writeFloat(float f, String tag) throws IOException;
-    public void writeDouble(double d, String tag) throws IOException;
-    public void writeString(String s, String tag) throws IOException;
-    public void writeBuffer(byte buf[], String tag)
-        throws IOException;
-    public void writeRecord(Record r, String tag) throws IOException;
-    public void startRecord(Record r, String tag) throws IOException;
-    public void endRecord(Record r, String tag) throws IOException;
-    public void startVector(List v, String tag) throws IOException;
-    public void endVector(List v, String tag) throws IOException;
-    public void startMap(TreeMap v, String tag) throws IOException;
-    public void endMap(TreeMap v, String tag) throws IOException;
+   public void writeByte(byte b, String tag) throws IOException;
+
+   public void writeBool(boolean b, String tag) throws IOException;
+
+   public void writeInt(int i, String tag) throws IOException;
+
+   public void writeLong(long l, String tag) throws IOException;
+
+   public void writeFloat(float f, String tag) throws IOException;
+
+   public void writeDouble(double d, String tag) throws IOException;
+
+   public void writeString(String s, String tag) throws IOException;
+
+   public void writeBuffer(byte buf[], String tag) throws IOException;
+
+   public void writeRecord(Record r, String tag) throws IOException;
+
+   public void startRecord(Record r, String tag) throws IOException;
+
+   public void endRecord(Record r, String tag) throws IOException;
+
+   public void startVector(List v, String tag) throws IOException;
+
+   public void endVector(List v, String tag) throws IOException;
+
+   public void startMap(TreeMap v, String tag) throws IOException;
+
+   public void endMap(TreeMap v, String tag) throws IOException;
 
 }
